3612c1ef81a70b24c6e856640b344b4b649f7a90
Vilma Kaijser <vilma.kaijser@gmail.com>
2021-04-13 15:09:52 +0200

    started on log in validation

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,5 +1,3 @@
-//$(document).ready(function(){
-
 
     jQuery.validator.addMethod("specialChrs", function (value, element) {
         return new RegExp('^[A-Za-zÀ-žÅÄÖåäö ]+$').test(value)
@@ -123,7 +121,38 @@
         }
     });
 
-    /**
+    
+   
+
+    function loginFormValidation(){
+        $('#loginForm').validate({
+
+            submitHandler:()=> { $('#submit').attr('disabled',true)},
+
+            rules : {
+                name: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 8,   
+                }
+            },
+            messages: {
+                name: {
+                    required: "Var god ange ditt användarnamn (e-mejladress)",
+                },
+                password: {
+                    required: "Var god ange ditt lösenord"
+                }
+            }
+        })
+
+    };
+
+
+     /**
      * 
      * Validates the form at the check out
      * 
@@ -184,5 +213,3 @@
           }
         })
     }
-
-//})
