6bac7ba1b5483bcf962162e50239210ed6cc4f1a
hodeiez <hodeinaiz@hotmail.com>
2021-04-27 14:27:29 +0200

    fixed to change the login icon href

--- js/sessionManager.js
+++ js/sessionManager.js
@@ -18,6 +18,7 @@ let changeTopVarOnloggedIn=()=>{
   
     if(profile!=null){
         $('#loggin-button').attr('href','profile.html')
+        $('#loggin-button-sidebar').attr('href','profile.html')
         //$('#navbar  .hakimLivs-design').after('<div class="col-4 text-center  ">Inloggad som:'+profile.name+'</div>');
        // $('#navbar').append('<button id="logout" class="col-2 btn shadow float-right mt-2 " style="height:30px" onClick="loggout();"><span class="align-middle text-white">Logga ut</span></button>')
         $('#navbar').append('<div class="row d-block ml-5 mr-1 ">Inloggad som:'+profile.name+'<button id="logout" class=" d-block btn shadow mt-1  " style="height:30px" onClick="loggout();"><span class=" text-white">Logga ut</span></button></div>')
