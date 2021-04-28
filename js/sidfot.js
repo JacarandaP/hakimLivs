3daced3179e6d791954937f9a742edbbe1bd12a3
lisaramel <lisa.ramel@yh.nackademin.se>
2021-04-28 14:08:19 +0200

    added icons

--- js/sidfot.js
+++ js/sidfot.js
@@ -6,5 +6,5 @@ const footerAddress="mockupdata/adminfooter.json"
 let footerData=()=>{
     fetch(footerAddress).then(resp=>resp.json()).then(json=> $('#footer').append(footerTemplate(json)))
 }
-let footerTemplate=(footer)=>`<div class="row bg-light pt-3 text-center"><div class="col ml-4"><b>email:</b> ${footer.email}</div><div class="col"><b>Telefon:</b> ${footer.phone}</div></div><div class=" text-center row bg-light pt-2 pb-2"><div class="col ml-4"><b>Address:</b> ${footer.address} ${footer.ort} ${footer.zip}</div><div class="col"><b>Öppetider:</b> ${footer.openfrom}-${footer.opentill}</div></div>`
+let footerTemplate=(footer)=>`<div class="row bg-light pt-4 text-center w3-blue-gray"><div class="col ml-4 bi bi-envelope-fill" style="font-size:1rem"><b></b> ${footer.email}</div><div class="col bi bi-telephone-fill" style="font-size:1rem"><b></b> ${footer.phone}</div></div><div class=" text-center row bg-light pt-2 pb-4 w3-blue-gray"><div class="col ml-4 bi bi-geo-alt-fill" style="font-size:1rem"><b></b> ${footer.address} ${footer.ort} ${footer.zip}</div><div class="col bi bi-clock-fill" style="font-size:1rem"><b></b> ${footer.openfrom} - ${footer.opentill}</div></div>`
 footerData()
